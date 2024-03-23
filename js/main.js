0651e025a85d5aa711bb32af244e68a11c661e05
Nico1109N <137794889+Nico1109N@users.noreply.github.com>
2024-03-23 10:14:17 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function caratula(){
+    let caratula= document.getElementById("modal-pelicula");
+    caratula.classList.add("active"); 
+    
+}
+function cerrar(){
+    let cerrar= document.getElementById("modal-pelicula");
+    cerrar.classList.remove("active"); 
+}
+function PanelLateral(){
+    let panel= document.getElementById("Panel-lateral");
+    panel.classList.add("active"); 
+}
+function cerrar2(){
+    let panel= document.getElementById("Panel-lateral");
+    panel.classList.remove("active"); 
+}
